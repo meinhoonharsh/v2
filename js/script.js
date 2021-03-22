4a2ae8e310de7184d8ff94e6c00b2a28b0240e53
Harsh Vishwakarma <harshprogrammer782@gmail.com>
2021-03-22 19:27:23 +0530

    Loader added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 AOS.init()
+$(window).ready(function() {
+    $('.loader01').hide();
+
+})
 $(window).scroll(function(e) {
     $el = $('.header01');
     $el.toggleClass('header_bar', $(this).scrollTop() > 0);
